c2c25c6d43a46a0ccaafd70cac28dc538552a608
Evgeny <evgeni3197@mail.ru>
2023-04-14 18:45:27 +0300

    change

--- robot_ampl_vol_sell.py
+++ robot_ampl_vol_sell.py
@@ -44,6 +44,7 @@ class Strategy:
                         now_vol_diff = 1
                     now_high_low = round((float(data['k']['h']) - float(data['k']['l'])) * 100 / float(data['k']['h']),2)
                     MA18 = statistics.mean(data_klines['data_close'][:-1] + [float(data['k']['c'])])
+                    MA9 = statistics.mean(data_klines['data_close'][:-1] + [float(data['k']['c'])])
                     if data['k']['x']:
 
                         """"""" Расчёт объёма """""""
@@ -55,7 +56,7 @@ class Strategy:
 
                         await asyncio.sleep(0.5)
 
-                    if average_volume*3 < float(data['k']['q']) and 0.4 < now_high_low < 2.5 and float(data['k']['c']) < MA18*(1 - (now_high_low+data_klines['average_diff']) * 0.01) and 0.1 < now_vol_diff < 0.7:
+                    if average_volume*3 < float(data['k']['q']) and 0.4 < now_high_low < 2.5 and float(data['k']['c']) < MA18*(1 - (now_high_low+data_klines['average_diff']) * 0.011) and float(data['k']['c']) < MA9*(1 - now_high_low * 0.011) and 0.1 < now_vol_diff < 0.7:
                         price_buy = float(data['k']['c'])
                         a = buy_order(self.pair, self.dollars_for_order, price_buy)
                         if a['position']:
@@ -63,7 +64,8 @@ class Strategy:
                             price_stop= a['entry_price'] * (1 - now_high_low * 0.004)
                             logger.info(f'{str(datetime.now())[8:19]}, {self.pair} цена {data["k"]["c"]}, vol/avg-vol= {round(float(data["k"]["q"]) / average_volume, 2)},'
                                         f'ampl= {now_high_low}, avg-ampl= {data_klines["average_diff"]}, vol_otnosh= {now_vol_diff},'
-                                        f'MA18= {MA18*(1 - (now_high_low+data_klines["average_diff"]) * 0.01)}')
+                                        f'MA18= {MA18*(1 - (now_high_low+data_klines["average_diff"]) * 0.011)}, '
+                                        f'MA9= {MA9 * (1 - now_high_low * 0.011)}')
                             position = True
                 while position:
                     data = json.loads(await client.recv())
