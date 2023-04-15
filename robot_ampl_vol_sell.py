96da177ac48f5c7624a0ab82ee2876d94e7e13ac
Evgeny <evgeni3197@mail.ru>
2023-04-15 08:53:35 +0300

    change

--- robot_ampl_vol_sell.py
+++ robot_ampl_vol_sell.py
@@ -56,12 +56,12 @@ class Strategy:
 
                         await asyncio.sleep(0.5)
 
-                    if average_volume*3 < float(data['k']['q']) and 0.4 < now_high_low < 2.5 and float(data['k']['c']) < MA18*(1 - (now_high_low+data_klines['average_diff']) * 0.011) and float(data['k']['c']) < MA9*(1 - now_high_low * 0.011) and 0.1 < now_vol_diff < 0.7:
+                    if average_volume*3 < float(data['k']['q']) and 0.4 < now_high_low < 2.5 and float(data['k']['c']) < MA18*(1 - (now_high_low+data_klines['average_diff']) * 0.012) and float(data['k']['c']) < MA9*(1 - now_high_low * 0.013) and 0.1 < now_vol_diff < 0.7:
                         price_buy = float(data['k']['c'])
                         a = buy_order(self.pair, self.dollars_for_order, price_buy)
                         if a['position']:
                             price_take = a['entry_price'] * (1 + now_high_low * 0.008)
-                            price_stop= a['entry_price'] * (1 - now_high_low * 0.004)
+                            price_stop= a['entry_price'] * (1 - now_high_low * 0.006)
                             logger.info(f'{str(datetime.now())[8:19]}, {self.pair} цена {data["k"]["c"]}, vol/avg-vol= {round(float(data["k"]["q"]) / average_volume, 2)},'
                                         f'ampl= {now_high_low}, avg-ampl= {data_klines["average_diff"]}, vol_otnosh= {now_vol_diff},'
                                         f'MA18= {MA18*(1 - (now_high_low+data_klines["average_diff"]) * 0.011)}, '
