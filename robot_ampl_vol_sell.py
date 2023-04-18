c611dd43eac82508246c195225c102baee4d6440
Evgeny <evgeni3197@mail.ru>
2023-04-18 22:56:18 +0300

    change

--- robot_ampl_vol_sell.py
+++ robot_ampl_vol_sell.py
@@ -30,7 +30,6 @@ class Strategy:
 
     async def main(self):
         list_volume = list(map(float, self.data_5m[7][19:29]))
-        average_volume = statistics.median(list_volume)
         data_klines = calculate_diff_first(self.data_5m)
         position = False
         url = f'wss://fstream.binance.com/ws/{self.pair.lower()}@kline_{self.interval}'
@@ -55,29 +54,33 @@ class Strategy:
 
                         await asyncio.sleep(0.5)
 
-                    if average_volume*7 < float(data['k']['q']) and 0.6 < now_high_low < 2.5 and float(data['k']['h']) < MA9 and float(data['k']['c']) < MA9*(1 - now_high_low * 0.01) and 0.1 < now_vol_diff < 0.7:
-                        price_buy = float(data['k']['c'])
-                        a = buy_order(self.pair, self.dollars_for_order, price_buy)
-                        if a['position']:
-                            price_take = a['entry_price'] * (1 + now_high_low * 0.01)
-                            price_stop= a['entry_price'] * (1 - now_high_low * 0.004)
-                            logger.info(f'{str(datetime.now())[8:19]}, {self.pair} цена {data["k"]["c"]}, vol/avg-vol= {round(float(data["k"]["q"]) / average_volume, 2)},'
-                                        f'ampl= {now_high_low}, avg-ampl= {data_klines["average_diff"]}, vol_otnosh= {now_vol_diff},'
-                                        f'MA9= {MA9*(1 - now_high_low * 0.01)},')
-                            position = True
+                        if average_volume*7 < float(data['k']['q']) and 0.6 < now_high_low < 2.5 and float(data['k']['h']) < MA9 and float(data['k']['c']) < MA9*(1 - now_high_low * 0.01) and 0.1 < now_vol_diff < 0.7:
+                            price_buy = float(data['k']['c'])
+                            a = buy_order(self.pair, self.dollars_for_order, price_buy)
+                            if a['position']:
+                                price_take = a['entry_price'] * (1 + now_high_low * 0.006)
+                                price_stop= a['entry_price'] * (1 - now_high_low * 0.006)
+                                position = True
                 while position:
                     data = json.loads(await client.recv())
                     if data['k']['x']:
                         list_volume = list_volume[1:] + [float(data['k']['q'])]
-                        average_volume = statistics.median(list_volume)
                         data_klines = calculate_diff(data, data_klines['list_diff'], data_klines['data_close'])
                     if float(data['k']['c']) >= price_take:
                         sell_order(self.pair, a['amt'])
-                        logger.info(f'{str(datetime.now())[8:19]}, {self.pair}, {data["k"]["c"]}, ---------TAKE_PROFIT---------')
+                        logger.info(
+                            f'take_profit, '
+                            f'{str(datetime.now())[8:19]}, {self.pair} цена {data["k"]["c"]}, vol/avg-vol= {round(float(data["k"]["q"]) / average_volume, 2)},'
+                            f'ampl= {now_high_low}, avg-ampl= {data_klines["average_diff"]}, vol_otnosh= {now_vol_diff},'
+                            f'MA9= {MA9 * (1 - now_high_low * 0.01)},')
                         position = False
                     if float(data['k']['c']) <= price_stop:
                         sell_order(self.pair, a['amt'])
-                        logger.info(f'{str(datetime.now())[8:19]}, {self.pair}, {data["k"]["c"]}, _________STOP_LOSS_________')
+                        logger.info(
+                            f'stop_loss, '
+                            f'{str(datetime.now())[8:19]}, {self.pair} цена {data["k"]["c"]}, vol/avg-vol= {round(float(data["k"]["q"]) / average_volume, 2)},'
+                            f'ampl= {now_high_low}, avg-ampl= {data_klines["average_diff"]}, vol_otnosh= {now_vol_diff},'
+                            f'MA9= {MA9 * (1 - now_high_low * 0.01)},')
                         position = False
 
 
