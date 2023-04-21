652617ef3d5fbf4830edc96056fc7e430e8d7e8c
Evgeny <evgeni3197@mail.ru>
2023-04-22 01:29:11 +0300

    change strategy

--- calculate_parametrs.py
+++ calculate_parametrs.py
@@ -23,21 +23,21 @@ def calculate_diff_volume(data, list_volume_diff):
 
 
 def calculate_diff_first(data_5m):
-    data_5m_high = list(data_5m[2][:30])
-    data_5m_low = list(data_5m[3][:30])
-    data_low_ma = list(map(float, data_5m[3][27:30]))
+    data_5m_high = list(data_5m[2][20:30])
+    data_5m_low = list(data_5m[3][20:30])
+    data_high_ma = list(map(float, data_5m[2][28:30]))
     list_diff = []
     for i in range(len(data_5m_high)):
         list_diff.append((float(data_5m_high[i]) - float(data_5m_low[i])) * 100 / float(data_5m_high[i]))
-    average_diff = round(statistics.median(list_diff), 2)
-    a = {'list_diff': list_diff, 'average_diff': average_diff, 'data_low_ma': data_low_ma}
+    average_diff = round(statistics.mean(list_diff), 2)
+    a = {'list_diff': list_diff, 'average_diff': average_diff, 'data_high_ma': data_high_ma}
     return a
 
 
-def calculate_diff(data, list_diff, data_low_ma):
+def calculate_diff(data, list_diff, data_high_ma):
     diff = (float(data['k']['h']) - float(data['k']['l'])) * 100 / float(data['k']['h'])
-    data_low_ma = data_low_ma[1:] + [float(data['k']['l'])]
+    data_high_ma = data_high_ma[1:] + [float(data['k']['h'])]
     list_diff = list_diff[1:] + [diff]
-    average_diff = round(statistics.median(list_diff), 2)
-    a = {'list_diff': list_diff, 'average_diff': average_diff, 'data_low_ma': data_low_ma}
+    average_diff = round(statistics.mean(list_diff), 2)
+    a = {'list_diff': list_diff, 'average_diff': average_diff, 'data_high_ma': data_high_ma}
     return a
---
+++ robot_ma_high.py
@@ -0,0 +1,91 @@
+import statistics
+from datetime import datetime
+import websockets
+import asyncio
+import json
+from binance.client import Client
+import pandas as pd
+from config import api, secret
+from calculate_parametrs import calculate_diff_first, calculate_diff
+from create_order import buy_order, sell_order
+from find_volatily_pairs import top_volatily
+import logging
+import sys
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+CLIENT = Client(api, secret)
+logging.basicConfig(stream=sys.stdout)
+logger = logging.getLogger('mylogger')
+logger.setLevel(logging.INFO)
+
+
+class Strategy:
+    def __init__(self, pair, interval, dollars):
+        self.pair = pair
+        self.interval = interval
+        self.data_5m = pd.DataFrame(CLIENT.futures_klines(symbol=self.pair, interval=self.interval, limit=30))
+        self.dollars_for_order = dollars
+
+    async def main(self):
+        data_klines = calculate_diff_first(self.data_5m)
+        MA2 = statistics.mean(data_klines['data_high_ma'])
+        position = False
+        url = f'wss://fstream.binance.com/ws/{self.pair.lower()}@kline_{self.interval}'
+        async with websockets.connect(url) as client:
+            while True:
+                while not position:
+                    data = json.loads(await client.recv())
+                    if data['k']['x']:
+
+                        """"""" Расчёт волатильности и скользящей средней """""""
+                        data_klines = calculate_diff(data, data_klines['list_diff'], data_klines['data_high_ma'])
+                        MA2 = statistics.mean(data_klines['data_high_ma'])
+                        print(f"ampl= {data_klines['average_diff']}, MA2= {MA2}")
+
+                        await asyncio.sleep(0.5)
+
+                    if float(data['k']['o']) <= float(data['k']['c']) < MA2*(1 - data_klines['average_diff'] * 0.03) and data_klines['average_diff'] > 0.19:
+                        price_buy = float(data['k']['c'])
+                        a = buy_order(self.pair, self.dollars_for_order, price_buy)
+                        if a['position']:
+                            price_take = min(a['entry_price'] * (1 + data_klines['average_diff'] * 0.02), a['entry_price']*1.01)
+                            price_stop= min(a['entry_price'] * (1 - data_klines['average_diff'] * 0.01), a['entry_price']*0.99)
+                            position = True
+                            avg_ampl1 = data_klines['average_diff']
+                            MA = MA2
+                            porog = (MA*(1-avg_ampl1*0.03) - price_buy) * 100 / MA*(1-avg_ampl1*0.03)
+                while position:
+                    data = json.loads(await client.recv())
+                    if data['k']['x']:
+                        data_klines = calculate_diff(data, data_klines['list_diff'], data_klines['data_high_ma'])
+                        MA2 = statistics.mean(data_klines['data_high_ma'])
+                    if float(data['k']['c']) >= price_take:
+                        sell_order(self.pair, a['amt'])
+                        logger.info(
+                            f'take_profit, {str(datetime.now())[8:19]}, {self.pair}, buy= {price_buy}, '
+                            f'MA2= {round(MA, 4)}, avg-ampl= {avg_ampl1}, porog= {round(porog, 4)}')
+                        position = False
+                    if float(data['k']['c']) <= price_stop:
+                        sell_order(self.pair, a['amt'])
+                        logger.info(
+                            f'stop_loss, {str(datetime.now())[8:19]}, {self.pair}, buy= {price_buy}, '
+                            f'MA2= {round(MA, 4)}, avg-ampl= {avg_ampl1}, porog= {round(porog, 4)}')
+                        position = False
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        for pair in top_volatily():
+            adp = Strategy(pair, '1m', 200)
+            asyncio.ensure_future(adp.main())
+        logger.info(f'start {datetime.now()}')
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.call_soon_threadsafe(loop.stop)
+        logger.info('Finished!')
+        loop.close()
